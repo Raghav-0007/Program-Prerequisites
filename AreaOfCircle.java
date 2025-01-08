0bf9193bd866f83e405fe603bbb353953f344b05
Raghav-0007 <rmyadav514@gmail.com>
2025-01-08 15:33:06 +0530

    Calculate area of circle program

---
+++ AreaOfCircle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class AreaOfCircle{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the radios of circle");
+double radios=sc.nextDouble();
+double Area=3.14*radios*radios;
+System.out.println("Area of circle: " + Area);
+}
+}
